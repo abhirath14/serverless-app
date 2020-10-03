4b7359179dcc5f092275bbef4ad6b7c79b768568
Shubham Sharma <shubham.sharma@iitrpr.ac.in>
2020-10-03 10:39:56 +0530

    comments added

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -5,6 +5,11 @@ const username = require('./settings.js').username;
 const password = require('./settings.js').password;
 const dbName = require('./settings.js').dbName;
 var _db;
+/**
+ * 
+ * @param {Object} app
+ * Connects to the mongodb atlas at a particular address with the required user credentials
+ */
 var mongoFunction = (app) => {
   const uri = `mongodb+srv://${username}:${password}@cluster0.5ejyo.mongodb.net/${dbName}?retryWrites=true&w=majority`;
   mongoose.connect(uri).then(() => {
